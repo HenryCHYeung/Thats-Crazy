9d1e248242b368a113880ae7015ad028fb647f91
Ahmadullah Sharifi <mayweatherwinslol@gmail.com>
2023-11-01 15:00:08 -0400

    missed it!

--- my-app/src/TypeChoice.js
+++ my-app/src/TypeChoice.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TypeChoice = ({ onChoiceSelected, visible }) => {
-  const handleChoice = (choice) => {
-    onChoiceSelected(choice);
+const TypeChoice = ({ onChoiceSelected }) => {
+  const handleChoice = (choice) => { //for getting the choice
+    onChoiceSelected(choice); 
   };
 
-  return (
-    <div id="type-choice" className={visible ? "" : "invisible"}>
+  return ( //getting the choice 
+    <div id="type-choice">
       <h1>What type of PC do you want?</h1>
       <button onClick={() => handleChoice("Gaming")}>Gaming</button>
       <button onClick={() => handleChoice("Production")}>Production</button>
