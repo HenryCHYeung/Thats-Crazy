79d11af6785e4d063b211bf3c2ffc13de6a9e3b4
Henry Yeung <yeungchihang22@gmail.com>
2023-11-11 00:11:38 -0500

    Updated database with new data

---
+++ server/tables.sql
@@ -0,0 +1,42 @@
+Create Table if not exists CPU(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    Socket VarChar(100) NOT NULL,
+    TDP TINYINT(100) NOT NULL,
+	PerformanceScore Integer NOT NULL
+);
+Create Table if not exists GPU(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    TDP TINYINT(100) NOT NULL,
+	PerformanceScore Integer NOT NULL
+);
+Create Table if not exists RAM(
+    Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    MemoryType VarChar(100) NOT NULL
+);
+Create Table if not exists Storage(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    Capacity VarChar(100) NOT NULL
+);
+Create Table if not exists PSU(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    Wattage SMALLINT(100) NOT NULL
+);
+Create Table if not exists Cases(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL
+);
+Create Table if not exists Motherboard(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL,
+    Socket VarChar(100) NOT NULL,
+    MemoryType VarChar(100) NOT NULL
+);
+Create Table if not exists CPUCooler(
+	Name VarChar(100) PRIMARY KEY,
+	Price Numeric(6,2) NOT NULL
+);
