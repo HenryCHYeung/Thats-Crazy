21d339ba44e3f02c5b419ff3760f0b635716cc13
Ahmadullah Sharifi <mayweatherwinslol@gmail.com>
2023-11-24 17:51:49 -0500

    added temp part site

--- my-app/src/PartList.js
+++ my-app/src/PartList.js
@@ -6,6 +6,24 @@ function PartList() {
   return (//Making stuff invisible + the conditional rendering between home, StorageChoice and typeChoice, home is just made invisible for simplicity.
   <div>
     <h3>All Parts</h3>
+    <h4>CPUs</h4>
+    <button>Show</button>
+
+    <h4>GPUs</h4>
+    <button>Cases</button>
+
+    <h4>Motherboards</h4>
+    <button>RAM</button>
+
+    <h4>PSUs</h4>
+    <button>Show</button>
+
+    <h4>Storage</h4>
+    <button>Show</button>
+
+    <h4>CPU Cooler</h4>
+    <button>Show</button>
+
   </div>
   );
 }
